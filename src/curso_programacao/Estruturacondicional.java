1b8843f0f3d6d340870cbdce27068e2bc5adf720
joaopedromateus <joaopmdas@gmail.com>
2024-12-02 20:28:43 -0300

    first commit

---
+++ src/curso_programacao/Estruturacondicional.java
@@ -0,0 +1,85 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Estruturacondicional {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+//		int hora;
+//		
+//		System.out.println("Quantas horas?");
+//		hora = sc.nextInt();		
+//		
+//		if (hora < 12) {
+//			System.out.println("Bom dia!");
+//		} else if(hora < 18) {
+//			System.out.println("Boa tarde");
+//		} else {
+//			System.out.println("Boa noite");
+//		}
+//			
+//				
+//		sc.close();		  
+
+//		int minutos = sc.nextInt();
+//		
+//		double conta = 50.0;
+//		if (minutos>100) {
+//			conta += (minutos-100) * 2;
+//		}
+//		
+//		System.out.printf("Valor da Conta = R$ %.2f%n", conta);
+//		
+//		
+		
+//		SWITCH-CASE
+//
+//		int x = sc.nextInt();
+//
+//		String dia;
+//		switch (x) {
+//		case 1:
+//			dia = "domingo";
+//			break;
+//		case 2:
+//			dia = "segunda";
+//			break;
+//		case 3:
+//			dia = "terca";
+//			break;
+//		case 4:
+//			dia = "quarta";
+//			break;
+//		case 5:
+//			dia = "quinta";
+//			break;
+//		case 6:
+//			dia = "sexta";
+//			break;
+//		case 7:
+//			dia = "sabado";
+//			break;
+//		default:
+//			dia = "valor invalido";
+//			break;
+//
+//		}
+//		System.out.println("Dia da semana: " + dia);
+//		sc.close();
+		
+		
+		
+//		Expressão Condicional Ternária
+		
+//		double preco = 34.5;
+//		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+//
+//		System.out.println(desconto);
+//		
+		
+	}
+}
---
+++ src/curso_programacao/Introducao.java
@@ -0,0 +1,121 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Introducao {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+//		String nome = "Maria";
+//		int idade = 31;
+//		double renda = 4000.0;
+//		double x = 10.35784;
+//		
+//		
+//		String product1 = "Computer";
+//		String product2 = "Office desk";
+//		int age = 30;
+//		int code = 5290;
+//		char gender = 'F';
+//		double price1 = 2100.0;
+//		double price2 = 650.50;
+//		double measure = 53.234567;
+//		
+//		
+//		
+//		System.out.printf("%.2f%n", x);
+//		System.out.printf("%.4f%n", x);
+//		System.out.print("Olá Mundo!");
+//		
+//		System.out.printf("%.4f%n", x);
+//		System.out.println("RESULTADO = " + x + " METROS");
+//		System.out.printf("RESULTADO = %.2f metros%n", x);
+//		
+//		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+//		
+//		
+//		System.out.println("________________________________________________________________________________________________");
+//		
+//		System.out.println("Products:");
+//		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
+//		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
+//		
+//		System.out.printf("Record: %d years old, code %d and gender: %s%n", age, code, gender);
+//		
+//		System.out.printf("Measure with eight decimal places:%.8f%n", measure);
+//		System.out.printf("Rouded(three decimal places): %.3f%n", measure);
+//		Locale.setDefault(Locale.US);
+//		System.out.printf("US decimal point: %.3f%n", measure);
+//		
+//		System.out.println("_______________________________________________________________________________________________");
+//		
+//		
+//		
+//		//Leitura de String
+//		String xyz;
+//		xyz = sc.next();
+//		System.out.println("Você Digitou: " + xyz);
+//		
+//		//Leitura de Inteiro
+//		int x;
+//		x = sc.nextInt();
+//		System.out.println("Você Digitou: " + x);
+//		
+//		                 
+//		//Leitura de Double
+//				double y;
+//				y = sc.nextDouble();
+//				System.out.println("Você Digitou: " + y);
+//				sc.close();
+//				                 
+//		
+//		________________________________________________________
+//
+//		String s1, s2, s3;
+//		
+//		s1 = sc.nextLine();
+//		s2 = sc.nextLine();
+//		s3 = sc.nextLine();
+//		
+//		System.out.println("DADOS DIGITADOS:");
+//		System.out.println(s1);
+//		System.out.println(s2);
+//		System.out.println(s3);
+//		sc.close();
+//	}
+		
+//		________________________________________________________
+//		
+//		int x, y, z;
+//		
+//		x = sc.nextInt();
+//		y = sc.nextInt();
+//		sc.close();
+//		z = x+y;
+//		
+//		System.out.println("SOMA = " + z);
+//		
+//		________________________________________________________
+		
+//		Locale.setDefault(Locale.US);
+//		
+//		double R, A, pi = 3.14159;
+//	    
+//	    R = sc.nextDouble();
+//
+//	    A = pi * R * R;
+//
+//	    System.out.printf("A=%.4f%n", A);
+//
+//		sc.close();
+//		
+//		________________________________________________________
+		
+		
+		
+		
+	}
+}
